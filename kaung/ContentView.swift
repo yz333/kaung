64407881285022387dbaf72d94e97a9690a8023c
kaungsathtun <khtun2@mail.ccsf.edu>
2020-04-12 18:09:08 -0700

    Initial Commit

---
+++ kaung/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kaung
+//
+//  Created by Kaung Sat Htun on 4/12/20.
+//  Copyright © 2020 Kaung Sat Htun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
